9c2e3992d478d168d623e27556dc420c91269570
j <mail@waterbrook.net>
2017-11-07 22:43:13 -0800

    reorganize some files and add react router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
-import App from './App';
+import App from './components/App';
 // TODO determine when to use the service worker for caching
 //import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'))
 // TODO (see above)
 //registerServiceWorker();
+
