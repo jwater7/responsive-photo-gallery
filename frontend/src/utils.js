e1d723db1166912325b670872079bf387f68c03f
j <mail@waterbrook.net>
2018-04-02 00:09:37 -0700

    add utils file

---
+++ frontend/src/utils.js
@@ -0,0 +1,18 @@
+// vim: tabstop=2 shiftwidth=2 expandtab
+//
+
+// Alternative to URLSearchParams for browsers that don't support it
+const getParams = query => {
+  if (!query) {
+    return { };
+  }
+
+  return (/^[?#]/.test(query) ? query.slice(1) : query)
+    .split('&')
+    .reduce((params, param) => {
+      let [ key, value ] = param.split('=');
+      params[key] = value ? decodeURIComponent(value.replace(/\+/g, ' ')) : '';
+      return params;
+    }, { });
+};
+
